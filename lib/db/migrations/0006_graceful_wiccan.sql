24f8525e77a3bc799277ec812d3accd78481ec87
Taehyeon Lim <plmoknijb3123@gmail.com>
2025-04-30 16:58:36 -0400

    Add role filed to user table

---
+++ lib/db/migrations/0006_graceful_wiccan.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "User" ADD COLUMN "role" varchar DEFAULT 'guest' NOT NULL;
--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -15,6 +15,9 @@ export const user = pgTable('User', {
   id: uuid('id').primaryKey().notNull().defaultRandom(),
   email: varchar('email', { length: 64 }).notNull(),
   password: varchar('password', { length: 64 }),
+  role: varchar('role', { enum: ['admin', 'user', 'guest'] })
+    .notNull()
+    .default('guest'),
 });
 
 export type User = InferSelectModel<typeof user>;
